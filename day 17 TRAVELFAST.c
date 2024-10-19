8cc90063240eae2b4ae8f9fc75de56caa5008f0a
divy754 <divyabohare7@gmail.com>
2024-10-19 19:09:43 +0530

    Add files via upload

---
+++ day 17 TRAVELFAST.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int T;  
+    int X, Y; 
+
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+        scanf("%d %d", &X, &Y);
+
+
+        if (X < Y) {
+            printf("BIKE\n");
+        } else if (X > Y) {
+            printf("CAR\n");
+        } else {
+            printf("SAME\n");
+        }
+    }
+
+    return 0;
+}
+
+
